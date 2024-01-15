93b2a964fa4bcc991f831bcfcd82b78e09b536da
Fabrizio Centeno <fcenteno322@gmail.com>
2024-01-15 10:35:59 +0100

    LineaFactura:credo

--- src/tienda/factura/LineaFactura.java
+++ src/tienda/factura/LineaFactura.java
@@ -1,4 +1,48 @@
 package tienda.factura;
 
+import tienda.stock.Producto;
+
 public class LineaFactura {
+    private Producto producto;
+    private int cantidad;
+    private double importelinea;
+
+    public LineaFactura(Producto p, int c) {
+        this.setProducto(p);
+        this.setCantidad(c);
+        this.setImportelinea(this.getProducto().getPrecioUnitario() * c);
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getImportelinea() {
+        return importelinea;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setImportelinea(double importelinea) {
+        this.importelinea = importelinea;
+    }
+
+
+    public String aCadena() {
+        return  this.getProducto().getRef() + "\t\t"
+                + this.getProducto().getDescripcion() + "\t\t\t"
+                + this.getCantidad() + "\t\t\t"
+                + this.getProducto().getPrecioUnitario() + "\t\t"
+                + this.getImportelinea();
+    }
 }
